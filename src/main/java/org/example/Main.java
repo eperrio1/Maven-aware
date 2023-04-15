cad57696a4ac7767c262c63ce38668a78b2703cd
eperrio1 <112588781+eperrio1@users.noreply.github.com>
2023-04-15 17:58:50 -0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+package Student.java;
+
+public class Main {
+//    public static void main(String[] args) {
+//        int numAdditiion = add(5, 6);
+//        System.out.print(numAdditiion);
+//    }
+    public static void main(String[] args){
+        add(1, 2);
+    }
+
+
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Student {
+    int num1;
+    int num2;
+
+    Student(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    int add(){
+        return this.num1 + this.add();
+    }
+}
